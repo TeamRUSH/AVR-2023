44cdea4cf222863f2627a47df412f3c1b039b364
asluchenba15 <asluchenba15@gmail.com>
2022-11-03 20:23:09 -0500

    Thursday Night

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -3,8 +3,15 @@
 # It also helps us make sure that our code is sending the proper payload on a topic
 # and is receiving the proper payload as well.
 from bell.avr.mqtt.client import MQTTModule
-from bell.avr.mqtt.payloads import AvrFcmVelocityPayload #,
-    #AvrApriltagsVisiblePayload
+from bell.avr.mqtt.payloads import (
+    AvrFcmVelocityPayload,
+    AvrApriltagsVisiblePayload,
+    AvrPcmSetServoOpenClosePayload,
+    AvrPcmSetServoPctPayload
+)
+import time
+import math
+
 
 # This imports the third-party Loguru library which helps make logging way easier
 # and more useful.
@@ -33,8 +40,12 @@ class Sandbox(MQTTModule):
         # find the associated capital. However, this does not work in reverse. So here,
         # we're creating a dictionary of MQTT topics, and the methods we want to run
         # whenever a message arrives on that topic.
-        self.topic_map = {"avr/fcm/velocity": self.show_velocity}
-        # self.topic_map = {"avr/fcm/velocity": self.show_velocity, "avr/apriltags/visible" : self.apriltag_visible}
+        # self.topic_map = {"avr/fcm/velocity": self.show_velocity}
+        self.topic_map = {"avr/fcm/velocity": self.show_velocity, "avr/apriltags/visible" : self.apriltag_visible, "avr/pcm/set_servo_open_close" : self.servo_buttons, "avr/pcm/set_servo_pct" : self.second_servo}
+        self.dump_fwd = True
+        self.dumped = False
+
+
 
     # Here's an example of a custom message handler here.
     # This is what executes whenever a message is received on the "avr/fcm/velocity"
@@ -56,20 +67,101 @@ class Sandbox(MQTTModule):
 
     # Here is an example on how to publish a message to an MQTT topic to
     # perform an action
-    def open_servo(self) -> None:
-        # It's super easy, use the `self.send_message` method with the first argument
-        # as the topic, and the second argument as the payload.
-        # Pro-tip, if you set `python.analysis.typeCheckingMode` to `basic` in you
-        # VS Code preferences, you'll get a red underline if your payload doesn't
-        # match the expected format for the topic.
+
+
+    def servo_buttons(self, payload: AvrPcmSetServoOpenClosePayload) -> None:
+        servo_id = payload["servo"]
+        command = payload["action"]
+        if servo_id == 0:
+            if command == "open":
+                if self.dump_fwd:
+                    self.servo_pct(4,100)
+                else:
+                    self.servo_pct(4,0)
+                self.dump_fwd = not self.dump_fwd
+
+            elif command == "close":
+                self.servo_pct(4,99)
+        elif servo_id == 1:
+            self.dumped = command == "close"
+
+    def second_servo(self, payload: AvrPcmSetServoPctPayload) -> None:
+        servo_id = payload["servo"]
+        percent = payload["percent"]
+        start = time.time()
+        if servo_id == 4:
+            if percent == 100:
+                end = start + 1.0
+                while time.time() < end:
+                    pass
+                self.servo_pct(4,55)
+
+            elif percent == 99:
+                end = start + 0.75
+                while time.time() < end:
+                    pass
+                self.servo_pct(4,0)
+
+            elif percent == 1:
+                end = start + 0.75
+                while time.time() < end:
+                    pass
+                self.servo_pct(4,100)
+
+            elif percent == 0:
+                end = start + 1.0
+                while time.time() < end:
+                    pass
+                self.servo_pct(4,55)
+
+
+    def servo_pct(self,id,pct):
         self.send_message(
-            "avr/pcm/set_servo_open_close",
-            {"servo": 0, "action": "open"},
+            "avr/pcm/set_servo_pct",
+            {"servo": id, "percent": pct}
         )
 
-"""     def apriltag_visible(self, payload: AvrApriltagsVisiblePayload) -> None:
-        a1 = payload["tags"]
-        logger.debug(f"Tag id: {a1}") """
+    def apriltag_visible(self, payload: AvrApriltagsVisiblePayload) -> None:
+        tags = payload["tags"]
+        dist = tags[0]["horizontal_dist"]
+        id = tags[0]["id"]
+        rel = tags[0]["pos_rel"]
+        X_raw = rel["x"]
+        Y_raw = rel["y"]
+        Z_raw = rel["z"]
+        head = math.radians(tags[0]["heading"])
+        X_rot = X_raw*math.sin(head)+Y_raw*math.cos(head)
+        Y_rot = X_raw*math.cos(head)+Y_raw*math.sin(head)
+
+        height = (-Z_raw*0.906307787)+(Y_rot*0.422618262)
+        f_dist = 41+(Z_raw*0.422618262)+(Y_rot*0.906307787)
+        s_dist = X_rot
+
+        abs_pos = (head, X_rot, Y_rot, f_dist, s_dist)
+        color = (0,0,0,0)
+        if id == 0:
+            color = (0,255,0,0)
+        elif id == 2:
+            color = (0,255,0,0)
+        elif id == 3:
+            color = (0,255,0,0)
+        elif id == 4:
+            color = (0,255,0,0)
+
+        if (abs(f_dist) <= 10.0) and (s_dist <= 20.0) and (s_dist >= 0.0):
+            color = (0,0,255,0)
+        elif (abs(f_dist) <= 10.0) and (s_dist <= 40.0) and (s_dist >= 20):
+            color = (0,0,0,255)
+        elif (abs(f_dist) <= 10.0) and (s_dist <= 0) and (s_dist >= -20.0) and not self.dumped:
+            color = (0,255,255,0)
+
+        self.send_message(
+                "avr/pcm/set_temp_color",
+                {"wrgb": color, "time": 0.35}
+        )
+
+
+        logger.debug(f"Pos: {abs_pos}")
 
 
 
@@ -85,3 +177,4 @@ if __name__ == "__main__":
     # The `run` method is defined by the inherited `MQTTModule` class and is a
     # convience function to start processing incoming MQTT messages infinitely.
     box.run()
+
